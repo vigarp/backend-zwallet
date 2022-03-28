0a74df4e542c275dcc798760613038c544d5dc70
vigarp <vigar.8421@outlook.com>
2022-03-28 10:10:57 +0700

    fix url react app url frontend

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const { Server } = require('socket.io');
 
 const io = new Server({
     cors: {
-        origin: 'http://localhost:3000'
+        origin: `${process.env.REACT_APP_URL_FRONTEND}`
     }
 });
 
